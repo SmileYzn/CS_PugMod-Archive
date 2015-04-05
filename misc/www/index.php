d8334c5194608fc65d24529279407fff86dee040
Cleverson 'SmileY' Rosalino da Silva Souza <SmileY@r7.com>
2015-04-05 00:45:06 -0300

    Added a HLTV page
    Added a HLTV page

--- misc/www/index.php
+++ misc/www/index.php
@@ -12,5 +12,21 @@
 		<form method="GET" action="top.php">
 			<input type="submit" value="TOP 10">
 		</form>
+		<br>
+		<br>
+		<form action="hltv.php" method="GET">
+			<span style="font-weight: bold; font-size: 1.1em;">Select an server: </span>
+			<select name="server">
+				<?php
+					include("config.php");
+
+					foreach($Servers as $Key => $Value)
+					{
+							echo "<option " . ($Key == $_GET['server'] ? "selected" : "") . " value='" . $Key . "'>" . $Value['title'] . "</option>";
+					}
+				?>
+			</select>
+			<button type="submit">Go</button>
+		</form>
 	</body>
 </html>
